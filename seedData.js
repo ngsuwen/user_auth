f6f4e9da0eef6a0098b60b2219544284ef4a2374
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2021-12-08 10:21:13 +0800

    admin details hidden

--- seedData.js
+++ seedData.js
@@ -1,10 +1,10 @@
 const User = require('./models/user')
 const bcrypt= require('bcrypt')
-
+require('dotenv').config();
 
 const upsertAdmin=async()=>{
-  const username='admin'
-  const password=await bcrypt.hash('admin',10)
+  const username=process.env.ADMIN_USER
+  const password=await bcrypt.hash(process.env.ADMIN_PASSWORD,10)
   await User.findOneAndUpdate(
     {
       username
